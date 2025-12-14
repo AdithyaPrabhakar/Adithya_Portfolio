4f935717ac256e3cbf5ae3b7245b150d74806d38
Adithya Prabhakar <132259264+AdithyaPrabhakar@users.noreply.github.com>
2025-12-14 23:08:30 +0530

    Latest commit

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-    server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  }
+    base: '/Adithya_Portfolio/',
+  
 })
